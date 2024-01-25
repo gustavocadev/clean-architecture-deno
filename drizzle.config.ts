2c40b7bf7ded84312929e9c4b10c6029dd7fe05a
gustavocadev <gustavocadev@gmail.com>
2024-01-25 12:56:31 -0500

    Fix import order in drizzle.config.ts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,5 +1,5 @@
-import type { Config } from 'npm:drizzle-kit';
 import process from 'node:process';
+import type { Config } from 'npm:drizzle-kit';
 
 // We need to make sure the in the tsconfig.json file, we need to change the target at least to 'ES6'
 export default {
