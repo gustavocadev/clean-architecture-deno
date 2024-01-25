0d4a754ea2ffff0817a24559bcf7fdcf80860383
gustavocadev <gustavocadev@gmail.com>
2024-01-25 12:49:16 -0500

    Update database connection in drizzle-db.ts

--- src/data/drizzle/drizzle-db.ts
+++ src/data/drizzle/drizzle-db.ts
@@ -1,14 +1,22 @@
-import { drizzle } from 'npm:drizzle-orm/better-sqlite3';
-import { migrate } from 'npm:drizzle-orm/better-sqlite3/migrator';
-import Database from 'npm:better-sqlite3';
+// import { drizzle } from 'drizzle-orm/better-sqlite3';
+// import { migrate } from 'drizzle-orm/better-sqlite3/migrator';
+// import Database from 'better-sqlite3';
+import { drizzle } from 'drizzle-orm/libsql';
+import { createClient } from '@libsql/client';
 import * as schema from './schema.ts';
 
-const betterSqlite = new Database(Deno.env.get('DB_URL'));
+const DATABASE_URL = Deno.env.get('DATABASE_URL') ?? '';
+const DATABASE_AUTH_TOKEN = Deno.env.get('DATABASE_AUTH_TOKEN') ?? '';
 
-export const db = drizzle(betterSqlite, {
+const client = createClient({
+  url: DATABASE_URL,
+  authToken: DATABASE_AUTH_TOKEN,
+});
+
+export const db = drizzle(client, {
   schema,
 });
 
 // this is important to bring the schema into the database, otherwise the tables won't be created
-migrate(db, { migrationsFolder: 'drizzle' });
-betterSqlite.close();
+// migrate(db, { migrationsFolder: 'drizzle' });
+// client.close();
