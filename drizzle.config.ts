08203f8c1fbc9f01f1b70cac9974b3b6b3f5050a
gustavocadev <gustavocadev@gmail.com>
2024-01-25 12:59:02 -0500

    Add dotenv dependency and update dbCredentials in drizzle.config.ts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,5 +1,6 @@
 import process from 'node:process';
 import type { Config } from 'npm:drizzle-kit';
+import 'npm:dotenv';
 
 // We need to make sure the in the tsconfig.json file, we need to change the target at least to 'ES6'
 export default {
@@ -8,5 +9,6 @@ export default {
   driver: 'turso',
   dbCredentials: {
     url: process.env.DB_URL!,
+    authToken: process.env.DB_AUTH_TOKEN!,
   },
 } satisfies Config;
